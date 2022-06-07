c0f659d3dfd9759d7fd226a142095e1281b59fc7
Chris Tian <wanying.tian@outlook.com>
2022-06-07 13:18:06 -0700

    changed path to virtual

--- code/ECG_features.py
+++ code/ECG_features.py
@@ -5,17 +5,25 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
+import sys
 
 # --- Importing Dataset ---
-df = pd.read_csv("D:/Documents/4B/BME499/github/BME499Project/datasets/ecg_2020-06-01.csv", header=9, usecols = ['Unit'])
-
+# df = pd.read_csv("D:/Documents/4B/BME499/github/BME499Project/datasets/ecg_2020-06-01.csv", header=9, usecols = ['Unit'])
+os.chdir("..") #move up one directory to BME 499
+our_path = os.path.abspath(os.curdir)
+our_path = our_path + '/datasets/ecg_2020-06-01.csv'
+print(our_path)
+df = pd.read_csv(our_path, header=9, usecols = ['Unit'])
 sampling_freq = df.iloc[6,0]
 #period = 1/sampling_freq
 
 #plot#
-x = np.arange(0, 30, 0.0019468)
+real_freq = len(df)/30
+period = 1/ real_freq
+x = np.arange(0, 30, period)
 y = df
-plt.plot(x, y)
+plt.plot(x, y.to_numpy())
   
 # naming the x axis
 plt.xlabel('Time(s)')
