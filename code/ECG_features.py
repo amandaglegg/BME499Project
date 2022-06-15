3a0360924b777a42cd1918a006925df0757a4b67
Christine Wong Chong <crw.wong@gmail.com>
2022-06-15 14:36:04 -0700

    Added Oldpeak and baseline functions

--- code/ECG_features.py
+++ code/ECG_features.py
@@ -4,6 +4,7 @@
 #Import libraries#
 
 import numpy as np
+from numpy import mean
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
@@ -18,7 +19,7 @@ def STprocess(ecg, freq):
     '''
     this function takes ecg and sampling frequency and return an array of values of start of ST segments
     '''
-       # Extract R-peaks locations
+    # Extract R-peaks locations
     _, rpeaks = nk.ecg_peaks(ecg, sampling_rate = freq)
     # Delineate
     signal, waves = nk.ecg_delineate(ecg, rpeaks, sampling_rate = freq, method="dwt", show=True, show_type='all')
@@ -84,26 +85,48 @@ def PR_baseline(ecg, freq):
     this can be used to calculate ST depression
     baseline  = average of ppeak and rpeak
     '''
+    # Extract R-peaks locations
+    _, rpeaks = nk.ecg_peaks(ecg, sampling_rate = freq)
+    # Delineate
+    signal, waves = nk.ecg_delineate(ecg, rpeaks, sampling_rate = freq, method="dwt", show=False, show_type='all')
+    #convert r peaks dictionary to list
+    indexR = list(rpeaks.values())
+    #removes the sampling rate from the list
+    indexR.pop()
+    #Get the values of the r peaks from the ecg 
+    dataR = []
+    i = 0 
+    while i < len(indexR):
+        dataR.append(ecg[indexR[i]]) #get the r peak values added to the list
+        i += 1
+    dataP = waves["ECG_P_Peaks"] # list of the samples that the p peaks occur
+    
+    ppeak =[] 
+    i = 0
+    while i < len(dataP):
+        ppeak.append (ecg[dataP[i]]) # adds the p peak values to the list
+        i += 1
+    averages = []
+    i = 0 
+    while i < len(dataR):
+        a = ((dataR[i] + ppeak[i])/2)
+        averages.append(a) # a list of the average of ppeaks and rpeaks
+        i += 1
+    baseline = mean(averages) # the average of the average values  
     baseline = 1
     print("in baseline()")
     return baseline
 
 def old_peak (rest_STstart, exercise_STstart, rest_baseline, exercise_baseline):
-    # %% Old peak -> difference between the rest and peak exercise ST depression 
-    # measured 0.8ms after the J point (in mV)
-    # Find the st segment in each one 
-    # a = ST depression in rest ecg = |STstart| - |STend|
-    # b = ST depression in exercise ecg = |STstart| - |STend|
-    # OP = |a| - |b| 
-    OP = 1
-    '''
-    If ST depression is measured by baseline to j point
-    1. Find STstart in each ecg 
-    2. Baseline is PR segment -> take the average of the Ppeaks and Ronsets?
-    3. a = ST depression in rest ecg = |baseline| - |STstart|
-    4. b = ST depression in exercise ecg = |baseline| - |STstart|
-    5. OP = |a| - |b|
-    '''
+    All_rest = []
+    for x in rest_STstart:
+        All_rest.append(abs(rest_baseline)- abs(rest_STstart)) #calculates ST depression
+    ST_dep_rest = mean(All_rest) # Averages the ST depression for the rest ecg
+    All_exercise = []
+    for x in exercise_STstart:
+        All_exercise.append(abs(exercise_baseline)- abs(exercise_STstart)) #calculates ST depression
+    ST_dep_exercise = mean(All_exercise) # Averages the ST depression for the exercise ecg
+    OP = (abs(ST_dep_rest)-abs(ST_dep_exercise))
     print("in oldpeak()")
     return OP
 #%%
@@ -169,3 +192,5 @@ plt.show()
         
 
 
+
+# %%
